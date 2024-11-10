72e6040c008840b230cd18a040eaa3ed87a88624
Ryan Culpepper <ryanc@racket-lang.org>
2024-11-10 04:50:21 -0500

    translate rich text to html

--- server.rkt
+++ server.rkt
@@ -3,9 +3,12 @@
 
 #lang racket/base
 (require racket/runtime-path
+         racket/match
+         racket/list
          racket/tcp
          web-server/servlet
          web-server/servlet-env
+         xml
          json
          "proof.rkt")
 (provide (all-defined-out))
@@ -52,7 +55,8 @@ Axiom 8: ∀ d,n ∈ NN, Divides(d,n) ⇔ (∃ k ∈ NN, n = k*d)
     (parameterize ((current-reject
                     (lambda (rt)
                       (define text (rich-text->string rt))
-                      (escape (hash 'v 1 'format "text" 'error text)))))
+                      (define html (xexpr->string ((rich-text->xexpr wrap-div) rt)))
+                      (escape (hash 'v 1 'format "html" 'error text 'errorh html)))))
       (with-handlers ([exn:fail?
                        (lambda (e)
                          (escape (hash 'v 1 'format "text" 'error (exn-message e))))])
@@ -65,7 +69,51 @@ Axiom 8: ∀ d,n ∈ NN, Divides(d,n) ⇔ (∃ k ∈ NN, n = k*d)
                 [else
                  `(p "OK. No errors found, but the proof is incomplete, because"
                      "the main list does not end with a Derive statement.")]))
-        (hash 'v 1 'format "text" 'pass (rich-text->string msg))))))
+        (define text (rich-text->string msg))
+        (define html (xexpr->string ((rich-text->xexpr wrap-div) msg)))
+        (hash 'v 1 'format "html" 'pass text 'passh html)))))
+
+(define ((rich-text->xexpr wrap) rt)
+  (match rt
+    [(? string? s) (wrap (list s))]
+    [(? rich? r) (wrap (list (rich->xexpr r)))]
+    [(cons 'v+ rts) (wrap (map (rich-text->xexpr wrap-div) rts))]
+    [(cons 'v rts) (wrap (map (rich-text->xexpr wrap-div) rts))]
+    [(cons 'h rts) (wrap (map (rich-text->xexpr wrap-span) rts))]
+    [(cons 'p rts) (wrap-p (add-between (map (rich-text->xexpr wrap-span) rts) " "))]))
+
+(define (wrap-div xs) `(div ([class "rt_v"]) ,@xs))
+(define (wrap-span xs) `(span ([class "rt_h"]) ,@xs))
+(define (wrap-p xs) `(div ([class "rt_p par"]) ,@xs))
+
+(define (rich->xexpr r)
+  (define (text) (rich->string r))
+  (match r
+    [(rich 'lineno ln) `(span ([class "r_lineno"]) ,(text))]
+    [(rich 'prop p) `(span ([class "r_prop"]) ,(text))]
+    [(rich 'expr e) `(span ([class "r_expr"]) ,(text))]
+    [(rich 'exprs es) `(span ([class "r_exprs"])
+                             ,@(add-between
+                                (for/list ([e (in-list es)])
+                                  `(span ([class "r_expr"]) ,(expr->string e)))
+                                ", "))]
+    [(rich 'ref (ref:axiom n))
+     `(span ([class "r_ref_ax"]) ,(text))]
+    [(rich 'ref (ref:line ln))
+     `(span ([class "r_ref_line"]) ,(text))]
+    [(rich 'ref #f) `(span ([class "r_ref_prop"]) (text))]
+    [(rich 'block-ref (ref:line ln))
+     `(span ([class "r_block_line"]) ,(text))]
+    [(rich 'var var) `(span ([class "r_var"]) ,(text))]
+    [(rich 'vars vs) `(span ([class "r_vars"])
+                            ,@(add-between
+                               (for/list ([v (in-list vs)])
+                                 `(span ([class "r_var"]) ,(symbol->string v)))
+                               ", "))]
+    [(rich 'pattern s) `(span ([class "r_pattern"]) ,(text))]
+    [(rich 'srcpair p) `(span ([class "r_srcpair"]) ,(text))]
+    [(rich 'rule s) `(span ([class "r_rule"]) ,(text))]
+    [_ `(span ([class "r_unknown"]) ,(text))]))
 
 ;; ============================================================
 
