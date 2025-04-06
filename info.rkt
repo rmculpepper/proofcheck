91a76be935fb753b507a65ae2d5e9d4a51145fa7
Ryan Culpepper <ryanc@racket-lang.org>
2025-04-06 13:56:28 -0400

    add info for pkg install

---
+++ info.rkt
@@ -0,0 +1,18 @@
+#lang info
+
+;; ========================================
+;; pkg info
+
+(define collection "proofcheck")
+(define version "1.0")
+
+(define deps
+  '("base"
+    "parser-tools-lib"
+    "rackunit-lib"
+    "web-server-lib"))
+
+;; ========================================
+;; collect info
+
+(define name "proofcheck")
